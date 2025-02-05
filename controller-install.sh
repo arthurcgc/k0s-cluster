a618c1b23fd0443bf6db20072869f2b37b7c6a39
Arthur Coelho <arthur.cavalcante.puc@gmail.com>
2025-02-05 19:25:03 -0300

    feat: add kratix helm chart

--- controller-install.sh
+++ controller-install.sh
@@ -6,7 +6,7 @@ sudo k0s kubeconfig admin > k0s_kubeconfig.yaml
 export KUBECONFIG=~/.kube/config:~/Documents/k0s-cluster/k0s_kubeconfig.yaml
 kubectl config view --flatten > config && mv config ~/.kube/config
 unset KUBECONFIG
-sudo k0s token create > join_token
-scp join_token sampi@192.168.0.23:/home/sampi/k0s/
-scp worker-install.sh sampi@192.168.0.23:/home/sampi/k0s/
-scp uninstall.sh sampi@192.168.0.23:/home/sampi/k0s/
+# sudo k0s token create > join_token
+# scp join_token sampi@192.168.0.23:/home/sampi/k0s/
+# scp worker-install.sh sampi@192.168.0.23:/home/sampi/k0s/
+# scp uninstall.sh sampi@192.168.0.23:/home/sampi/k0s/
